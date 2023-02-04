2133ac981d21f4a76420a7b4d374be2a2297eb57
jejikeh <jejikeh@gmail.com>
2023-02-04 03:08:09 +0300

    added new example

--- src/Wooff.Examples/Entities/Dog.cs
+++ src/Wooff.Examples/Entities/Dog.cs
@@ -1,22 +1,35 @@
 using Wooff.ECS;
-using Wooff.ECS.Component;
 using Wooff.ECS.Entity;
 using Wooff.Examples.Components.CoreComponent;
 
 namespace Wooff.Examples.Entities;
 
-public class Dog : Entity<int>
+public class Dog : Entity<DogData>
 {
-    public int Del = 1;
+    private string _name = "not set";
+    private int _age;
     
     public Dog()
     {
         Add<Transform>(x => new Transform((float)x[0], (float)x[1], (float)x[2]), 3f, 4f, 5f);
     }
 
-    public override IInitable<int> Init(params int[] data)
+    public override IInitable<DogData> Init(params DogData[] data)
     {
-        Del = data[0];
+        foreach(var dogData in data){
+            _name = dogData.Name;
+            _age = dogData.Age;
+        }
+            
         return this;
     }
+
+    public void WhoAmIm(){
+        Console.WriteLine($"Name: {_name}\nAge: {_age}");
+    }
+}
+
+public class DogData {
+    public string Name {get; set;} = "Not set";
+    public int Age {get; set;}
 }
--- src/Wooff.Examples/Program.cs
+++ src/Wooff.Examples/Program.cs
@@ -1,17 +1,34 @@
-﻿using Wooff.ECS.Component;
-using Wooff.Examples.Components.CoreComponent;
+﻿using Wooff.Examples.Components.CoreComponent;
 using Wooff.Examples.Entities;
 using Wooff.Examples.Worlds;
 
-var world = new Park();
-var dog = world.EntityContext
-    .Add<Dog, int>(2)
-        .Add<Speaker, SpeakerData>(new SpeakerData()
-        {
-            Message = "Hello World"
-        });
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var world = new Park();
+        foreach(var i in Enumerable.Range(0,10))
+            world.EntityContext.Add<Dog, DogData>(new DogData() {
+                Name = HashToString(new Random().Next(100000000)),
+                Age = ((char)((byte)new Random().Next()))
+            }).Add<Speaker, SpeakerData>(new SpeakerData()
+                {
+                    Message = "Hello World"
+                });
 
-foreach (var tempDog in world.EntityContext.GetAll<Dog>())
-    tempDog?.Get<Speaker>().Speak();
+        foreach (var tempDog in world.EntityContext.GetAll<Dog>()) {
+            tempDog?.Get<Speaker>().Speak();
+            tempDog?.WhoAmIm();
+        }
+    }
 
+    private static string HashToString(int hash, int asciiShift = 97, int asciiWidth = 17){
+        var random = new Random();
+        var resultString = string.Empty;
+        var digits = hash == 0L ? 1 : (hash > 0L ? 1 : 2) + (int)Math.Log10(Math.Abs((double)hash));
+        foreach(var _ in Enumerable.Range(0, digits))
+            resultString += (char)(((int)(hash / (Math.Pow(10, random.Next(digits)))) % 10) + (random.Next(asciiWidth) + asciiShift));
 
+        return resultString;
+    }
+}
