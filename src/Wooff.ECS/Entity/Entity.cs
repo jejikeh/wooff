b2be3408956c6098e9b923332dcfcb5fa414acbf
jejikeh <jejikeh@gmail.com>
2023-02-03 16:24:24 -0800

    components now can be in entity in one instance per type

--- src/Wooff.ECS/Entity/Entity.cs
+++ src/Wooff.ECS/Entity/Entity.cs
@@ -3,13 +3,49 @@ using Wooff.ECS.Context;
 
 namespace Wooff.ECS.Entity;
 
-public abstract class Entity : Context<IComponent>, IEntity
+public abstract class Entity : Context<IComponent>, IEntity<IComponent>
 {
+    public new IComponent Add(IComponent item)
+    {
+        if (Contains(item))
+            throw new ArgumentException($"Component {item.GetType().FullName} is already attached to entity {GetType().FullName}");
+
+        base.Add(item);
+        return item;
+    }
+
+    public new T1 Add<T1>(params object[] data) where T1 : IComponent, IInitable, new()
+    {
+        var item = IInitable.Initialize<T1>(data) as IComponent;
+        if (item is not T1 parsedComponent || Contains<T1>())
+            throw new ArgumentException($"Component {item.GetType().FullName} is already attached to entity {GetType().FullName}");
+        
+        Add(parsedComponent);
+        return parsedComponent;
+    }
+
+    public new T1 Add<T1, T2>(params T2[] data) where T1 : IComponent, IInitable<T2>, new()
+    {
+        var item = IInitable<T2>.Initialize<T1>(data) as IComponent;
+        if (item is not T1 parsedComponent || Contains<T1>())
+            throw new ArgumentException($"Component {item.GetType().FullName} is already attached to entity {GetType().FullName}");
+        
+        Add(parsedComponent);
+        return parsedComponent;
+    }
+    
+    // TODO: add new overrides for ContextAdd method
+
     public virtual void Update(float timeScale) { }
     
     public virtual Task UpdateParallelAsync(float timeScale)
     { 
         return Task.Run(() => Update(timeScale));
+    }
+    
+    public virtual IInitable Init(params object[] data)
+    {
+        return this;
     }
 
     public IInitable<IComponent> Init(params IComponent[] data)
@@ -19,13 +55,9 @@ public abstract class Entity : Context<IComponent>, IEntity
 
         return this;
     }
+}
 
-    public IInitable Init(params object[] data)
-    {
-        foreach (var temp in data)
-            if(temp is IComponent component)
-                Add(component);
-
-        return this;
-    }
+public abstract class Entity<T> : Entity, IEntity<T>
+{
+    public abstract IInitable<T> Init(params T[] data);
 }
--- src/Wooff.ECS/Entity/IEntity.cs
+++ src/Wooff.ECS/Entity/IEntity.cs
@@ -3,6 +3,10 @@ using Wooff.ECS.Context;
 
 namespace Wooff.ECS.Entity;
 
-public interface IEntity : IContext<IComponent>, IUpdateable, IInitable<IComponent>
+public interface IEntity : IContext<IComponent>, IUpdateable, IInitable
+{
+}
+
+public interface IEntity<T> : IEntity, IInitable<T>
 {
 }
