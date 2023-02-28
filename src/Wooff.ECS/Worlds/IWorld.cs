b41b659715b4234bcd484b7068b63cb6e6aca9ed
jejikeh <jejikeh@gmail.com>
2023-03-01 00:15:21 +0300

    WIP remove interface generics

--- src/Wooff.ECS/Worlds/IWorld.cs
+++ src/Wooff.ECS/Worlds/IWorld.cs
@@ -1,15 +1,10 @@
-using System.Collections.Generic;
 using Wooff.ECS.Contexts;
-using Wooff.ECS.Entities;
-using Wooff.ECS.Systems;
 
 namespace Wooff.ECS.Worlds
 {
-    public interface IWorld<T, T1> 
-        where T : IEntity<T>
-        where T1 : ISystem<T>
+    public interface IWorld
     {
-        public IContext<T, List<T>> EntityContext { get; }
-        public IContext<T1, HashSet<T1>> SystemContext { get; }
+        public EntityContext EntityContext { get; }
+        public SystemContext SystemContext { get; }
     }
 }
