c350b0783b4bea49bb4dcb6111bc0d75e43c81fb
jejikeh <jejikeh@gmail.com>
2023-03-01 00:18:00 +0300

    Entity now stores component in dictionary

--- src/Wooff.ECS/Entities/IEntity.cs
+++ src/Wooff.ECS/Entities/IEntity.cs
@@ -3,9 +3,9 @@ using Wooff.ECS.Contexts;
 
 namespace Wooff.ECS.Entities 
 {
-    public interface IEntity<T> : IHashContext<IComponent<IConfig, T>>, IContextItem
-        where T : IEntity<T>
+    public interface IEntity : 
+        IContext<IComponent>, 
+        IContextQueryable<IComponent>
     {
-        
     }
 }
