e2ceb95c6c289c448c1c790ce33a415b3599910d
jejikeh <jejikeh@gmail.com>
2023-03-01 00:17:41 +0300

    WIP SystemContext implenent new IContext

--- src/Wooff.ECS/Contexts/SystemContext.cs
+++ src/Wooff.ECS/Contexts/SystemContext.cs
@@ -1,17 +1,46 @@
 using System.Collections.Generic;
-using Wooff.ECS.Contexts;
+using System.Linq;
 using Wooff.ECS.Entities;
+using Wooff.ECS.Systems;
 
-namespace Wooff.ECS.Systems 
+namespace Wooff.ECS.Contexts 
 {
-    public class SystemContext<T, T1> : Context<T, HashSet<T>>, IHashContext<T>, IProcessable<IContext<T1, List<T1>>>, IContextItem 
-        where T : ISystem<T1>
-        where T1 : IEntity<T1>
+    public class SystemContext : IContext<ISystem>, IProcessable<IContext<IEntity>>
     {
-        public void Process(float timeScale, IContext<T1, List<T1>> data)
+        private readonly HashSet<ISystem> _systems;
+
+        public SystemContext(params ISystem[] systems)
         {
-            foreach(var item in Items)
-                item.Process(timeScale, data);
+            _systems = new HashSet<ISystem>();
+            foreach (var system in systems)
+                ContextAdd(system);
+        }
+        
+        public ISystem ContextAdd(ISystem item)
+        {
+            _systems.Add(item);
+            return item;
+        }
+
+        public T1? ContextGet<T1>() where T1 : class, ISystem
+        {
+            return _systems.FirstOrDefault(x => x.GetType().IsAssignableFrom(typeof(T1))) as T1;
+        }
+
+        public bool ContextContains<T1>() where T1 : class, ISystem
+        {
+            return ContextGet<T1>() is null;
+        }
+
+        public bool ContextRemove(ISystem item)
+        {
+            return _systems.Remove(item);
+        }
+
+        public void Process(float timeScale, IContext<IEntity> context)
+        {
+            foreach (var system in _systems)
+                system.Process(timeScale, context);
         }
     }
 }
