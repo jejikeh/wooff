785f6bbd002e8c48fdb1c0fa69ed149d675953fd
jejikeh <jejikeh@gmail.com>
2023-03-01 00:15:58 +0300

    IComponent now blank

--- src/Wooff.ECS/Components/IComponent.cs
+++ src/Wooff.ECS/Components/IComponent.cs
@@ -1,13 +1,6 @@
-using Wooff.ECS.Contexts;
-using Wooff.ECS.Entities;
-
 namespace Wooff.ECS.Components
 {
-    public interface IComponent<T, out T1> : IContextItem, IConfigurable<T> 
-        where T : IConfig
-        where T1 : IEntity<T1>
+    public interface IComponent
     {
-        public T1 Handler { get; } 
-        public void OnRemove();
     }
 }
