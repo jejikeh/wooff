7da3a73f3cda89043ac534ebf9fe21a2795b5c46
jejikeh <jejikeh@gmail.com>
2023-03-01 00:16:52 +0300

    simplified IContext

--- src/Wooff.ECS/Contexts/IContext.cs
+++ src/Wooff.ECS/Contexts/IContext.cs
@@ -1,7 +1,10 @@
 ﻿namespace Wooff.ECS.Contexts
 {
-    public class IContext
+    public interface IContext<T>
     {
-        
+        public T ContextAdd(T item);
+        public T1? ContextGet<T1>() where T1 : class, T;
+        public bool ContextContains<T1>() where T1 : class, T;
+        public bool ContextRemove(T item);
     }
 }
