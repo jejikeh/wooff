4e1e103192dda857bcebaa743ed0f6a89cb758ae
jejikeh <jejikeh@gmail.com>
2023-03-01 00:15:09 +0300

    implement interface ISystem

--- src/Wooff.ECS/Systems/System.cs
+++ src/Wooff.ECS/Systems/System.cs
@@ -1,7 +1,25 @@
-﻿namespace Wooff.ECS.Systems
+﻿using System;
+using Wooff.ECS.Contexts;
+using Wooff.ECS.Entities;
+
+namespace Wooff.ECS.Systems
 {
-    public class System
+    public abstract class System : ISystem
     {
-        
+        public void Process(float timeScale, IContext<IEntity> data)
+        {
+            if(data is IContextQueryable<IEntity> contextQueryableEntity)
+                UpdateFromEntityQuery(timeScale, contextQueryableEntity);
+            if(data is IContextQueryable<Type> contextQueryableType)
+                UpdateFromTypeQuery(timeScale, contextQueryableType);
+        }
+
+        public virtual void UpdateFromEntityQuery(float timeScale, IContextQueryable<IEntity> context)
+        {
+        }
+
+        public virtual void UpdateFromTypeQuery(float timeScale, IContextQueryable<Type> context)
+        {
+        }
     }
 }
